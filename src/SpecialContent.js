a8c5c3c6bc45d9d4d995e2a7d8a52bac6f8cc865
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2021-09-23 00:15:37 +0300

    lab14

---
+++ src/SpecialContent.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class SpecialContent extends Component {
+	render() {
+		return <div>{this.props.text}</div>;
+	}
+}
+
+export default SpecialContent;
